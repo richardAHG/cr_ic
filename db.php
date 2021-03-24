ace8f7db586826c974d2991e3fce5072696f1b80
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-03-24 01:10:24 -0500

    se modifico gitignore

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=db_conference',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    
+    // 'on afterOpen' => function ($event) {
+    //     $event->sender->createCommand("SET timezone='America/Lima';")->execute();
+    // },
+
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=dbrbac',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
+    
+];
