8aa45ac361f1c290b0f35604b8ccf9ac32e954e9
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-03-25 12:53:32 -0500

    se agrego validacion de CORs en index.php

--- controllers/DiarysController.php
+++ controllers/DiarysController.php
@@ -8,33 +8,21 @@ class DiarysController extends ActiveController
 {
     public $modelClass = 'app\models\custom\DiarycustomModel';
 
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-
-        // remove authentication filter necessary because we need to 
-        // add CORS filter and it should be added after the CORS
-        unset($behaviors['authenticator']);
-
-        // add CORS filter
-        $behaviors['corsFilter'] = [
-            'class' => '\yii\filters\Cors',
-            'cors' => [
-                'Origin' => ['*'],
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                'Access-Control-Request-Headers' => ['*'],
-            ],
-        ];
-
-        // // re-add authentication filter of your choce
-        // $behaviors['authenticator'] = [
-        //     'class' => yii\filters\auth\HttpBasicAuth::class
-        // ];
-
-        // // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
-        // $behaviors['authenticator']['except'] = ['options'];
-        return $behaviors;
-    }
+    // public function behaviors()
+    // {
+    //     $behaviors = parent::behaviors();
+    //     unset($behaviors['authenticator']);
+    //     // add CORS filter
+    //     $behaviors['corsFilter'] = [
+    //         'class' => '\yii\filters\Cors',
+    //         'cors' => [
+    //             'Origin' => ['*'],
+    //             'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    //             'Access-Control-Request-Headers' => ['*'],
+    //         ],
+    //     ];
+    //     return $behaviors;
+    // }
 
     public function actions()
     {
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
