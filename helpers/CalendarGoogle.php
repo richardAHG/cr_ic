b55b721d1732a2d28d4271c0d9bccb389b3b6b4e
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-04-10 10:22:06 -0500

    test de registro de evento

--- helpers/CalendarGoogle.php
+++ helpers/CalendarGoogle.php
@@ -80,7 +80,37 @@ class CalendarGoogle
             'overrides' => ['method' => 'popup', 'minutes' => 10]
           ]
         ];
-        print_r($infoEvent); die();
+
+        $event = new Google_Service_Calendar_Event(array(
+      'summary' => 'Google I/O 2015',
+      'location' => '800 Howard St., San Francisco, CA 94103',
+      'description' => 'A chance to hear more about Google\'s developer products.',
+      'start' => array(
+        'dateTime' => '2021-05-10T09:00:00-07:00',
+        'timeZone' => 'America/Los_Angeles',
+      ),
+      'end' => array(
+        'dateTime' => '2021-05-10T17:00:00-07:00',
+        'timeZone' => 'America/Los_Angeles',
+      ),
+      'recurrence' => array(
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ),
+      'attendees' => array(
+        array('email' => 'richard21hg92@gmail.com'),
+        array('email' => 'richard@cuborojo.pe'),
+      ),
+      'reminders' => array(
+        'useDefault' => FALSE,
+        'overrides' => array(
+          array('method' => 'email', 'minutes' => 24 * 60),
+          array('method' => 'popup', 'minutes' => 10),
+        ),
+      ),
+    ));
+
+        print_r($infoEvent); 
+        print_r($event); die();
         $event = new Google_Service_Calendar_Event($infoEvent);
         $calendarId = 'primary';
         $resultEvent = $service->events->insert($calendarId, $event);
