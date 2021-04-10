0350b8fbecabee055eedefc864fa794b199e80c9
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-04-10 10:30:31 -0500

    test de registro de evento

--- helpers/CalendarGoogle.php
+++ helpers/CalendarGoogle.php
@@ -61,58 +61,58 @@ class CalendarGoogle
           ];
         }
 
-        $start = [
-          'dateTime' => '2021-05-10T09:00:00-07:00',
-          'timeZone' => 'America/lima'
-        ];
-        $end = [
-          'dateTime' => '2021-05-10T09:00:00-07:00',
-          'timeZone' => 'America/lima'
-        ];
-        $infoEvent = [
-          'summary' => $value['title'],
-          'location' => $value['city'],
-          'description' => $value['description'],
-          'start' => $start,
-          'end' => $end,
-          'attendees' => $arraySpeakers,
-          'reminders' => [
-            'useDefault'=>FALSE,
-            'overrides' => ['method' => 'popup', 'minutes' => 10]
-          ]
-        ];
+        // $start = [
+        //   'dateTime' => '2021-05-10T09:00:00-07:00',
+        //   'timeZone' => 'America/lima'
+        // ];
+        // $end = [
+        //   'dateTime' => '2021-05-10T09:00:00-07:00',
+        //   'timeZone' => 'America/lima'
+        // ];
+        // $infoEvent = [
+        //   'summary' => $value['title'],
+        //   'location' => $value['city'],
+        //   'description' => $value['description'],
+        //   'start' => $start,
+        //   'end' => $end,
+        //   'attendees' => $arraySpeakers,
+        //   'reminders' => [
+        //     'useDefault'=>FALSE,
+        //     'overrides' => ['method' => 'popup', 'minutes' => 10]
+        //   ]
+        // ];
 
-    //     $event = array(
-    //   'summary' => 'Google I/O 2015',
-    //   'location' => '800 Howard St., San Francisco, CA 94103',
-    //   'description' => 'A chance to hear more about Google\'s developer products.',
-    //   'start' => array(
-    //     'dateTime' => '2021-05-10T09:00:00-07:00',
-    //     'timeZone' => 'America/Los_Angeles',
-    //   ),
-    //   'end' => array(
-    //     'dateTime' => '2021-05-10T17:00:00-07:00',
-    //     'timeZone' => 'America/Los_Angeles',
-    //   ),
-    //   'recurrence' => array(
-    //     'RRULE:FREQ=DAILY;COUNT=2'
-    //   ),
-    //   'attendees' => array(
-    //     array('email' => 'richard21hg92@gmail.com'),
-    //     array('email' => 'richard@cuborojo.pe'),
-    //   ),
-    //   'reminders' => array(
-    //     'useDefault' => FALSE,
-    //     'overrides' => array(
-    //       array('method' => 'email', 'minutes' => 24 * 60),
-    //       array('method' => 'popup', 'minutes' => 10),
-    //     ),
-    //   ),
-    // );
+        $event = array(
+      'summary' => 'Google I/O 2015',
+      'location' => '800 Howard St., San Francisco, CA 94103',
+      'description' => 'A chance to hear more about Google\'s developer products.',
+      'start' => array(
+        'dateTime' => '2021-05-10T09:00:00-07:00',
+        'timeZone' => 'America/Los_Angeles',
+      ),
+      'end' => array(
+        'dateTime' => '2021-05-10T17:00:00-07:00',
+        'timeZone' => 'America/Los_Angeles',
+      ),
+      'recurrence' => array(
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ),
+      'attendees' => array(
+        array('email' => 'richard21hg92@gmail.com'),
+        array('email' => 'richard@cuborojo.pe'),
+      ),
+      'reminders' => array(
+        'useDefault' => FALSE,
+        'overrides' => array(
+          array('method' => 'email', 'minutes' => 24 * 60),
+          array('method' => 'popup', 'minutes' => 10),
+        ),
+      ),
+    );
 
         // print_r($infoEvent); 
         // print_r($event); die();
-        $event = new Google_Service_Calendar_Event($infoEvent);
+        $event = new Google_Service_Calendar_Event($event);
         $calendarId = 'primary';
         $resultEvent = $service->events->insert($calendarId, $event);
         echo $resultEvent->htmlLink;
