2f0a0428a26638f52a6f4628cf6e9f44e2079047
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-06-30 16:38:17 -0500

    se agreo expand para mostrar ruta del archivo

--- models/custom/ParticipantsCustomModel.php
+++ models/custom/ParticipantsCustomModel.php
@@ -6,5 +6,18 @@ use app\models\ParticipantsModel;
 
 class ParticipantsCustomModel extends ParticipantsModel
 {
+    public function extraFields()
+    {
+        return ['file'];
+    }
 
+    public function getFile()
+    {
+        return (new \Yii\db\Query())
+            ->select(['f.route'])
+            ->from('participants p')
+            ->join('INNER JOIN', 'files f', 'p.photo_id =f.id and f.`status` =1')
+            ->where('p.condition = 1 and p.id = :id', [':id' => $this->id])
+            ->scalar();
+    }
 }
