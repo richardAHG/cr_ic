36df830fa914fbaa692509850cecdabd41ce83bd
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-07-01 00:33:49 -0500

    se corrigio la fomra de mostrar la ruta del archivo

--- models/custom/ParticipantsCustomModel.php
+++ models/custom/ParticipantsCustomModel.php
@@ -14,7 +14,7 @@ class ParticipantsCustomModel extends ParticipantsModel
     public function getFile()
     {
         return (new \Yii\db\Query())
-            ->select(['f.route'])
+            ->select(["substr(f.route,position('media/' in f.route)) as route"])
             ->from('participants p')
             ->join('INNER JOIN', 'files f', 'p.photo_id =f.id and f.`status` =1')
             ->where('p.condition = 1 and p.id = :id', [':id' => $this->id])
