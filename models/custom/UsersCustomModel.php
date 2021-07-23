52df24c34edc37b1ec05fee369c50c56e9176296
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-07-23 09:49:30 -0500

    se elimino extrafields de usercustom model y se paso a pasticipantcustom

--- models/custom/UsersCustomModel.php
+++ models/custom/UsersCustomModel.php
@@ -9,27 +9,5 @@ use Yii;
 
 class UsersCustomModel extends UsersModel
 {
-    public function extraFields()
-    {
-        return ['EventsbySpeaker', 'EventsbyModerator'];
-    }
-
-    public function getEventsbySpeaker()
-    {
-        $requestParams = Yii::$app->getRequest()->getQueryParams();
-
-        $eventIds = UsuarioQuery::getEventsBySpeaker($requestParams['id']);
-
-        $evento = EventsQuery::getEventById($eventIds);
-        return EventsQuery::getEventsByIds($evento);
-    }
-    public function getEventsbyModerator()
-    {
-        $requestParams = Yii::$app->getRequest()->getQueryParams();
-
-        $eventIds = UsuarioQuery::getEventsByModerator($requestParams['id']);
-
-        $evento = EventsQuery::getEventById($eventIds);
-        return EventsQuery::getEventsByIds($evento);
-    }
+   
 }
