4d09647e9761f9668ed212be46d8c014f8492398
Richard Huáman Gómez <richard21hg92@gmail.com>
2021-07-23 09:25:49 -0500

    se agrego expand de eventbyspeaker y eventbymoderator

--- models/custom/ParticipantsCustomModel.php
+++ models/custom/ParticipantsCustomModel.php
@@ -3,12 +3,15 @@
 namespace app\models\custom;
 
 use app\models\ParticipantsModel;
+use app\models\query\EventsQuery;
+use app\models\query\UsuarioQuery;
+use Yii;
 
 class ParticipantsCustomModel extends ParticipantsModel
 {
     public function extraFields()
     {
-        return ['file'];
+        return ['file', 'EventsbySpeaker', 'EventsbyModerator'];
     }
 
     public function getFile()
@@ -19,5 +22,24 @@ class ParticipantsCustomModel extends ParticipantsModel
             ->join('INNER JOIN', 'files f', 'p.photo_id =f.id and f.`status` =1')
             ->where('p.condition = 1 and p.id = :id', [':id' => $this->id])
             ->scalar();
+    }
+
+    public function getEventsbySpeaker()
+    {
+        $requestParams = Yii::$app->getRequest()->getQueryParams();
+
+        $eventIds = UsuarioQuery::getEventsBySpeaker($requestParams['id']);
+
+        $evento = EventsQuery::getEventById($eventIds);
+        return EventsQuery::getEventsByIds($evento);
+    }
+    public function getEventsbyModerator()
+    {
+        $requestParams = Yii::$app->getRequest()->getQueryParams();
+
+        $eventIds = UsuarioQuery::getEventsByModerator($requestParams['id']);
+
+        $evento = EventsQuery::getEventById($eventIds);
+        return EventsQuery::getEventsByIds($evento);
     }
 }
